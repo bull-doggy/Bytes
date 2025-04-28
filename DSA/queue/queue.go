26c1ac45864f6900170559ca395e2cab070e97be
bull-doggy <camerondevv@gmail.com>
2025-04-28 15:11:53 +0530

    queue package init

---
+++ DSA/queue/queue.go
@@ -0,0 +1,8 @@
+package queue
+
+// enqueue(x)	         Add an element x to the rear (end) of the queue.
+// dequeue()	         Remove and return the front element from the queue.
+// peek() / front()     View (but not remove) the front element.
+// isEmpty()	         Check whether the queue has no elements.
+// size()	             Get the number of elements in the queue.
+
