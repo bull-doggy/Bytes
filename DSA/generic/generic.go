f6b1fa4dac5ec4ed1c578e151226351c2ed94dc8
bull-doggy <camerondevv@gmail.com>
2025-05-04 20:43:37 +0530

    generic module added where based on operetion seperated

---
+++ DSA/generic/generic.go
@@ -0,0 +1,21 @@
+package generic
+
+type Ordered interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64 |
+		~string
+}
+
+type Numeric interface {
+	Integer | Float
+}
+
+type Float interface {
+	~float32 | ~float64
+}
+
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
